467d7500ba54a0a1625879db85b17e6341b44929
robotStepheny <s_perez@nmsu.edu>
2015-03-09 09:34:01 -0600

    Create GeneticAlgorithm.h

---
+++ GeneticAlgorithm.h
@@ -0,0 +1,28 @@
+/*
+ * GeneticAlgorithm.h
+ *
+ *  Created on: Feb 27, 2015
+ *      Author: Stepheny
+ */
+
+#ifndef GENETICALGORITHM_H_
+#define GENETICALGORITHM_H_
+
+#include "Population.h"
+#include "Path.h"
+class GeneticAlgorithm {
+public:
+	GeneticAlgorithm();
+	Population evolvePopulation(Population);
+	Path crossover(Path&, Path&);
+	void mutate(Path);
+	static Path& tournamentSelection(Population);
+
+private:
+	static const double mutationRate = 0.02;
+	static const int tournamentSize = 5;
+	static const bool elitism = true;
+
+};
+
+#endif /* GENETICALGORITHM_H_ */
